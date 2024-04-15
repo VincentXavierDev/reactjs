5e303ade6040b91ff889eb16e1c09a479ce3b6cf
Vinh Dao Xuan (SDC8.BU1) <vinh.dao@ntq-solution.com.vn>
2024-04-15 15:14:29 +0700

    done js core

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,42 +1,56 @@
 // Get data from API
-import React, { Component } from 'react'
-import Loading from './Loading'
-import axios from 'axios'
+import React, { useState } from 'react'
+// import axios from 'axios'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: [],
-      loading: true
-    }
+function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
   }
-
-  async getUser() {
-    const res = await axios('https://api.randomuser.me/?nat=US&result=5')
-    this.setState({
-      user: res.data.results,
-      loading: false
-    })
-  }
-
-  // Chạy sau khi component được mounted -- tương tự với hook mounted trong vuejs
-  async componentDidMount() {
-    await this.getUser()
-  }
-
-  clickHandle() {
-    console.log("helooooooooooooooooooooo");
-  }
-
-  render() {
-    return <div className='App'>
-      {this.state.loading ? <Loading mess={"hehehehehehe"} /> : this.state.user.map(u => <div key={u}>{u.cell}</div>)}
-      <button style={{ color: "red" }} onClick={this.clickHandle}>
-        Click here!
+  return (
+    <div>
+      <button style={{ color: "red" }} onClick={increment}>
+        Click {count} times!
       </button>
     </div>
-  }
+  )
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       user: [],
+//       loading: true
+//     }
+//   }
+
+//   async getUser() {
+//     const res = await axios('https://api.randomuser.me/?nat=US&result=5')
+//     this.setState({
+//       user: res.data.results,
+//       loading: false
+//     })
+//   }
+
+//   // Chạy sau khi component được mounted -- tương tự với hook mounted trong vuejs
+//   async componentDidMount() {
+//     await this.getUser()
+//   }
+
+//   clickHandle() {
+//     console.log("helooooooooooooooooooooo");
+//   }
+
+//   render() {
+//     return <div className='App'>
+//       {this.state.loading ? <Loading mess={"hehehehehehe"} /> : this.state.user.map(u => <div key={u}>{u.cell}</div>)}
+//       <button style={{ color: "red" }} onClick={this.clickHandle}>
+//         Click here!
+//       </button>
+//     </div>
+//   }
+// }
 
 export default App;
