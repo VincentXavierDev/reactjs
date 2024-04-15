740574bd348299192eaffa0dd4b38f48c94a0022
NTQ-SOLUTION\vinh.dao <vinh.dao@ntq-solution.com.vn>
2024-04-15 11:24:51 +0700

    done js core

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Get data from API
+import React, { Component } from 'react'
+import Loading from './Loading'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hê lô world
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: [],
+      loading: true
+    }
+  }
+
+  async getUser() {
+    const res = await axios('https://api.randomuser.me/?nat=US&result=5')
+    this.setState({
+      user: res.data.results,
+      loading: false
+    })
+  }
+
+  // Chạy sau khi component được mounted -- tương tự với hook mounted trong vuejs
+  async componentDidMount() {
+    await this.getUser()
+  }
+
+  clickHandle() {
+    console.log("helooooooooooooooooooooo");
+  }
+
+  render() {
+    return <div className='App'>
+      {this.state.loading ? <Loading mess={"hehehehehehe"} /> : this.state.user.map(u => <div key={u}>{u.cell}</div>)}
+      <button style={{ color: "red" }} onClick={this.clickHandle}>
+        Click here!
+      </button>
     </div>
-  );
+  }
 }
 
 export default App;
